1ec773b668188c573b2ed36297c3d7fb0b21c3ee
Mohan K <151464177+nhm20@users.noreply.github.com>
2025-01-25 23:32:49 +0530

    1

---
+++ client/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const routes = require("./routes");
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// -------------- Deployment ----------------
+const __dirname1 = path.resolve();
+// Serve the frontend build in production
+app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+// Handle all routes to serve the index.html
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+);
+// -------------- Deployment ----------------
+
+const port = process.env.PORT || 3000;
+app.use("/api", routes);
+
+app.listen(port, () => {
+  console.log(`Server listening at ${port}`);
+});
